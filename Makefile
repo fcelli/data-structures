a25a9aeb920c6ae1306cd8b2ab907a7e1029b540
fcelli <fedecel93@gmail.com>
2025-01-18 15:35:40 +0000

    test: add hash table tests

--- Makefile
+++ Makefile
@@ -6,11 +6,16 @@ CFLAGS = -Wall -Wextra -g -Iinclude
 SRC_DIR = src
 INCLUDE_DIR = include
 BUILD_DIR = build
+TESTS_DIR = tests
+TESTS_BUILD_DIR = $(BUILD_DIR)/tests
 
 # Files
 SRCS = $(wildcard $(SRC_DIR)/*.c)
 OBJS = $(SRCS:$(SRC_DIR)/%.c=$(BUILD_DIR)/%.o)
 TARGET = $(BUILD_DIR)/libdatastructures.a
+TEST_SRCS = $(wildcard $(TESTS_DIR)/*.c)
+TEST_OBJS = $(TEST_SRCS:$(TESTS_DIR)/%.c=$(TESTS_BUILD_DIR)/%.o)
+TEST_BINS = $(TEST_SRCS:$(TESTS_DIR)/%.c=$(TESTS_BUILD_DIR)/%)
 
 # Rules
 all: $(TARGET)
@@ -23,7 +28,19 @@ $(BUILD_DIR)/%.o: $(SRC_DIR)/%.c
 	@mkdir -p $(BUILD_DIR)
 	$(CC) $(CFLAGS) -c $< -o $@
 
+$(TESTS_BUILD_DIR)/%.o: $(TESTS_DIR)/%.c
+	@mkdir -p $(TESTS_BUILD_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+tests: $(TARGET) $(TEST_BINS)
+	@for test in $(TEST_BINS); do \
+		$$test; \
+	done
+
+$(TESTS_BUILD_DIR)/%: $(TESTS_BUILD_DIR)/%.o $(TARGET)
+	$(CC) $(CFLAGS) $< -L$(BUILD_DIR) -ldatastructures -o $@
+
 clean:
 	rm -rf $(BUILD_DIR)
 
-.PHONY: all clean
+.PHONY: all tests clean
---
+++ tests/test_hashtable.c
@@ -0,0 +1,50 @@
+#include "hashtable.h"
+#include <assert.h>
+
+uint64_t simple_hash(const char* key) {
+    uint64_t hash = 0;
+    while (*key) {
+        hash = (hash << 5) + hash + *key++;
+    }
+    return hash;
+}
+
+void print_test_passed(const char* test_name) {
+    printf("\t- %s: \033[1;32mPASSED\033[0m\n", test_name);
+}
+
+void test_hash_table_create_destroy() {
+    hash_table* ht = hash_table_create(10, simple_hash);
+    assert(ht != NULL);
+    hash_table_destroy(ht);
+    print_test_passed(__func__);
+}
+
+void test_hash_table_insert_lookup() {
+    hash_table* ht = hash_table_create(10, simple_hash);
+    assert(hash_table_insert(ht, "key1", (void*)1) == true);
+    assert(hash_table_insert(ht, "key2", "value") == true);
+    void* value = hash_table_lookup(ht, "key1");
+    assert(value == (void*)1);
+    value = hash_table_lookup(ht, "key2");
+    assert(strcmp(value, (void*)"value") == 0);
+    hash_table_destroy(ht);
+    print_test_passed(__func__);
+}
+
+void test_hash_table_delete() {
+    hash_table* ht = hash_table_create(10, simple_hash);
+    hash_table_insert(ht, "key", (void*)1);
+    assert(hash_table_delete(ht, "key") == (void*)1);
+    assert(hash_table_lookup(ht, "key") == NULL);
+    hash_table_destroy(ht);
+    print_test_passed(__func__);
+}
+
+int main(int argc, char** argv) {
+    printf("Running tests in: %s\n", argv[0]);
+    test_hash_table_create_destroy();
+    test_hash_table_insert_lookup();
+    test_hash_table_delete();
+    return 0;
+}
